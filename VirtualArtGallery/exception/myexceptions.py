cf14d3a5cf4a60127b8697155c70e81a179d4f46
sai prabath <saiprabathcse@gmail.com>
2024-05-01 20:51:09 +0530

    ADD: exceptions

---
+++ VirtualArtGallery/exception/myexceptions.py
@@ -0,0 +1,30 @@
+class ArtWorkNotFoundException(Exception):
+    def __init__(self, message="Artwork not found."):
+        self.message = message
+        super().__init__(self.message)
+
+class UserNotFoundException(Exception):
+    def __init__(self, message="User not found."):
+        self.message = message
+        super().__init__(self.message)
+
+class ArtistNotFoundException(Exception):
+    def __init__(self, message="Artist not found."):
+        self.message = message
+        super().__init__(self.message)
+
+class GalleryNotFoundException(Exception):
+    def __init__(self, message="Gallery not found."):
+        self.message = message
+        super().__init__(self.message)
+
+
+
+#except ArtWorkNotFoundException as e:
+#    print("Artwork not found:", e.message)
+
+#except UserNotFoundException as e:
+#    print("User not found:", e.message)
+
+#except Exception as e:
+#    print("An error occurred:", str(e))

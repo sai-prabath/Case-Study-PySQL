fa245b9391a105b0e62c8c520e967895bcd1a449
sai prabath <saiprabathcse@gmail.com>
2024-05-03 09:32:11 +0530

    ADD: DB connectivity with file handling

--- VirtualArtGallery/util/dbutil.py
+++ VirtualArtGallery/util/dbutil.py
@@ -1,19 +1,27 @@
 import mysql.connector
+from mysql.connector import Error
 
 class DBConnection:
+    connection = None
+
     @staticmethod
     def getConnection():
         try:
-            connection = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="Prabath.sql@0000",
-                port="3306",
-                database="TBS"
-            )
-            if connection.is_connected():
-                print("Connected to MySQL database")
-                return connection
-        except mysql.connector.Error as e:
-            print("Error connecting to MySQL database:", e)
+            if DBConnection.connection is None or not DBConnection.connection.is_connected():
+                connection_string = PropertyUtil.getPropertyString("/Users/saiprabathchowdary/Documents/hexaware/segue foundation -python/Assignment/Case-Study-PySQL/VirtualArtGallery/util/DBdata.txt")
+                DBConnection.connection = mysql.connector.connect(**connection_string)
+            return DBConnection.connection
+        except Error as e:
+            print("Error:", e)
             return None
+
+
+class PropertyUtil:
+    @staticmethod
+    def getPropertyString(property_file):
+        properties = {}
+        with open(property_file,'r') as file:
+            for line in file:
+                key, value = line.strip().split('=')
+                properties[key] = value
+        return properties
